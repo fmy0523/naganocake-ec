9db0003d2d0a040225b2d797679cb05c44ab858c
vagrant <vagrant@localhost.localdomain>
2020-10-31 13:56:16 +0000

    Add

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -12,10 +12,16 @@ class Admin::MembersController < ApplicationController
     @member = Customer.find(params[:id])
   end
 
+  def update
+    @member = Customer.find(params[:id])
+    @member.update(customer_params)
+    redirect_to admin_member_path(@member.id)
+  end
+
    private
    # ストロングパラメータ
    def customer_params
-     params.require(:customer).permit(:last_name, :first_name, :email, :is_deleted)
+     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_number, :email, :is_deleted)
    end
 
 end
