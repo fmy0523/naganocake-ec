b66b0e8214a666c2ee2a9561e0d592919497dd81
vagrant <vagrant@localhost.localdomain>
2020-11-19 14:18:18 +0000

    Add

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -18,12 +18,34 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
+    # 新たにカートに追加した商品を＠cart.itemに入れる。
     @cart_item = CartItem.new(cart_item_params)
     @cart_item.customer_id = current_customer.id
-    @cart_item.save
-    # 商品をカートに入れたらカート画面へ遷移
-    redirect_to public_cart_items_path
-  end
+    @cart_item_u = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+    # ログインカスタマーのカートに既に同じ商品があるかどうか。カートに同じ商品がある場合、
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+      # 同一商品の個数に足す
+      new_quantity = @cart_item_u.quantity + @cart_item.quantity
+      @cart_item_u.update_attribute(:quantity, new_quantity)
+      # 新しく入って来た商品は消す
+      @cart_item.delete
+    else
+      # 新たにカートに入れる。
+      @cart_item.save
+    end
+      redirect_to public_cart_items_path
+   end
+
+
+
+  # def create
+  #   @cart_item = CartItem.new(cart_item_params)
+  #   @cart_item.customer_id = current_customer.id
+  #   @cart_item.save
+  #   # 商品をカートに入れたらカート画面へ遷移
+  #   redirect_to public_cart_items_path
+  # end
+
 
 
   def destroy
@@ -39,10 +61,6 @@ class Public::CartItemsController < ApplicationController
     redirect_to public_cart_items_path
   end
 
-    # if @cart_item.item.destroy
-     # flash[:notice] = "item was successfully destroyed."
-     # redirect_to cart_item_path
-    # end
 
 
 
